2edc7b4147141d8851738bcebf6e92a88f74ee4e
Erick-Meza <erick.mezavilleda25@ycdsbk12.ca>
2024-02-15 12:36:15 -0500

    Completed Power assignment

--- Power.java
+++ Power.java
@@ -1,13 +1,27 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that will ask the user for a base and an exponent and print out the result
+  * @author: Erick M
   */
   
   public void run() {
     
-    // start coding here
+    // declares variables
+    double dblBase;
+    double dblExponent;
+    double dblResult;
+
+    // prompts user for values
+    dblBase = readDouble("Please enter a value for the base: ");
+    dblExponent = readDouble("Please enter a value for the exponent: ");
+
+    // calculates the operation
+    dblResult = Math.pow(dblBase, dblExponent);
+
+    // outputs the resulting value
+    System.out.println("The resultant is " + dblResult);
+    
     
   }
 }
